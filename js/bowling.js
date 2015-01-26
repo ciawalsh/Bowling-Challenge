1bf7e6ec963485ff6cc63e355b96b1518d177f78
ciawalsh <cia.walsh@gmail.com>
2015-01-26 15:42:24 +0000

    Has a strike bonus

--- js/bowling.js
+++ js/bowling.js
@@ -1,12 +1,3 @@
-// var Scoreboard = function(player) {
-// 	this.defaultScore = 0;
-// 	this.player = player;
-// };
-
-// Scoreboard.prototype.currentScore = function() {
-// 	return this.player.currentScore;
-// };
-
 var Player = function() {
 	this.playerName = 'default';
 	this.defaultScore = 0;
@@ -15,69 +6,100 @@ var Player = function() {
 	this.attemptScore = 0;
 	this.frameScore = 0;
 	this.attemptNumber = 1;
+	this.strikeBonus = false;
+	this.spareBonue = false;
 };
 
 Player.prototype.enterName = function(enteredName) {
 	this.playerName = enteredName || prompt("What is your name?");
 };
 
-// Player.prototype.startGame = function() {
-// 	player.enterName();
-// };
+Player.prototype.updateScore = function(pinsHit) {
+	if (this.strikeBonus === false) {
+		if (this.attemptNumber === 1) {
+			score = prompt("What did you score on the first attempt?") || pinsHit;
+			if (score < 10) {
+				this.attemptScore = score;
+				this.currentScore += parseInt(score);
+				this.attemptNumberChange();
+			} else if (score == 10) {
+				this.frameScore = score;
+				this.attemptScore = score;
+				this.currentScore += parseInt(score);
+				this.activateStrikeBonus();
+				this.endFrame();
+			} else {
+				alert("Please enter a number between 1-10");
+			}
+		}
+		else if (this.attemptNumber === 2) {
+			var score2 = prompt("What did you score the second attempt?") || pinsHit;
+			if (parseInt(score) + parseInt(score2) < 10) {
+				this.attemptScore = score2;
+				this.frameScore = parseInt(score) + parseInt(score2);
+				this.currentScore += parseInt(score2);
+				this.attemptNumberChange();
+				this.endFrame();
+			} else if (parseInt(score) + parseInt(score2) == 10) {
+				this.frameScore = 10;
+				this.attemptScore = score2;
+				this.currentScore += parseInt(score2);
+				this.attemptNumberChange();
+				this.endFrame();
+			} else {
+				alert("Please enter a number between 1-10");
+			}
+		}
+	}
+	else if (this.strikeBonus === true) {
+		if (this.attemptNumber === 1) {
+			score = prompt("What did you score on the first attempt?") || pinsHit;
+			if (score < 10) {
+				this.attemptScore = score;
+				this.currentScore += (parseInt(score)*2);
+				this.attemptNumberChange();
+			} else if (score == 10) {
+				this.frameScore = score;
+				this.attemptScore = score;
+				this.currentScore += (parseInt(score)*2);
+				this.endFrame();
+			} else {
+				alert("Please enter a number between 1-10");
+			}
+		}
+		else if (this.attemptNumber === 2) {
+			var score2 = prompt("What did you score the second attempt?") || pinsHit;
+			if (parseInt(score) + parseInt(score2) < 10) {
+				this.attemptScore = score2;
+				this.frameScore = parseInt(score) + parseInt(score2);
+				this.currentScore += (parseInt(score)*2);
+				this.attemptNumberChange();
+				this.activateStrikeBonus();
+				this.endFrame();
+			} else if (parseInt(score) + parseInt(score2) == 10) {
+				this.frameScore = 10;
+				this.attemptScore = score2;
+				this.currentScore += (parseInt(score)*2);
+				this.attemptNumberChange();
+				this.activateStrikeBonus();
+				this.endFrame();
+			} else {
+				alert("Please enter a number between 1-10");
+			}
+		}
+	}
+};
 
-// Player.prototype.strike = function() {
-// 	if (this.currentFrame < 10) {
-// 		this.currentScore += 10;
-// 		this.frameScore = 10;
-// 		this.calculateFrameScore();
-// 		this.endFrame();
+// Player.prototype.calculateFrameScore = function() {
+// 	if (this.frameScore == 10) {
+// 		var answer = prompt("Did you get a strike or spare?");
+// 		if (answer == "strike") {
+// 			return "Well done";
+// 		} else {
+// 			return "Just as good";
+// 		}
 // 	}
 // };
-
-Player.prototype.updateScore = function(pinsHit) {
-	if (this.attemptNumber === 1) {
-		score = prompt("What did you score on the first attempt?") || pinsHit;
-		if (score < 10) {
-			this.attemptScore = score;
-			this.currentScore += parseInt(score);
-			this.attemptNumberChange();
-		} else if (score == 10) {
-			this.frameScore = score;
-			this.currentScore += parseInt(score);
-			this.endFrame();
-		} else {
-			alert("Please enter a number between 1-10");
-		}
-	}
-	else if (this.attemptNumber === 2) {
-		var score2 = prompt("What did you score the second attempt?") || pinsHit;
-		if (parseInt(score) + parseInt(score2) < 10) {
-			this.attemptScore = score2;
-			this.frameScore = parseInt(score) + parseInt(score2);
-			this.currentScore += parseInt(score2);
-			this.attemptNumberChange();
-			this.endFrame();
-		} else if (parseInt(score) + parseInt(score2) == 10) {
-			this.frameScore = 10;
-			this.currentScore += parseInt(score2);
-			this.attemptNumberChange();
-			this.endFrame();
-		} else {
-			alert("Please enter a number between 1-10");
-		}
-	}
-};
-
-Player.prototype.calculateFrameScore = function() {
-	if (this.frameScore == 10) {
-		var answer = prompt("Did you get a strike or spare?");
-		if (answer == "strike") {
-			return "Well done";
-		} else {
-			return "Just as good";
-		}
-	}
-};
 
 Player.prototype.attemptNumberChange = function () {
 	if (this.attemptNumber == 1) {
@@ -92,5 +114,13 @@ Player.prototype.endFrame = function() {
 		this.currentFrame += 1;
 	} else if (this.currentFrame === 10) { 
 		this.totalScore = this.currentScore; 
+	}
+};
+
+Player.prototype.activateStrikeBonus = function() {
+	if (this.strikeBonus === false) {
+		this.strikeBonus = true;
+	} else {
+		this.strikeBonus = false;
 	}
 };
