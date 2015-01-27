cb9801678873a1fc9c6bd4f700cd94bab6735fec
ciawalsh <cia.walsh@gmail.com>
2015-01-27 10:39:11 +0000

    Added basic css

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,32 +1,3 @@
-// describe ("Scoreboard", function() {
-
-// 	var scoreboard;
-// 	var player;
-
-// 	beforeEach(function() {
-// 		player = new Player();
-// 		scoreboard = new Scoreboard(player);
-// 	});
-
-// 	describe("by default", function() {
-
-// 		it("shows no score", function() {
-// 			expect(scoreboard.defaultScore).toEqual(0);
-// 		});
-
-// 	});
-
-// 	describe("is able to show a score", function() {
-
-// 		it("after a hitting 1 pin down", function() {
-// 			player.bowl();
-// 			expect(scoreboard.currentScore()).toEqual(1);
-// 		});
-
-// 	});
-
-// });
-
 describe ("Player", function() {
 
 	var player;
@@ -48,26 +19,16 @@ describe ("Player", function() {
 
 		it("and it show on the board", function() {
 			player.updateScore(5);
-			expect(player.currentScore).toEqual(5);
+			expect(player.attemptScore).toEqual('5');
 		});
 
-		it("and it show in two sections within one frame", function() {
+		it("and it show on the board", function() {
 			player.updateScore(5);
-			expect(player.currentScore).toEqual(5);
+			player.updateScore(5);
+			expect(player.frameScore).toEqual(10);
 		});
-
+		
 	});
-
-	// describe("can update score", function() {
-
-	// 	it("and it show on the board", function() {
-	// 		player.updateScore(5);
-	// 		expect(player.currentScore).toEqual(5);
-	// 	});
-
-	// });
-
-
 
 });
 
